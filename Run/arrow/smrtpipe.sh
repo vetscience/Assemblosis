682a41192f8aeae73fd3de6c6cd1070b47aa9979
Pasi Korhonen <pasi.korhonen@unimelb.edu.au>
2018-07-23 10:03:46 +1000

    Adjustment to udocker

--- Run/arrow/smrtpipe.sh
+++ Run/arrow/smrtpipe.sh
@@ -67,7 +67,7 @@ touch dummy.fastq
 ngmlr -r /root/smrtlink/install/smrtlink-release_5.0.1.9585/bundles/smrttools/install/smrttools-release_5.0.1.9578/private/pacbio/pythonpkgs/pbcore/lib/python2.7/site-packages/pbcore/data/datasets/$PREFIX/sequence/$PREFIX.fasta -q dummy.fastq -o dummy.bam -t 1
 SEQCNT=`grep ">" $ASSEMBLY | wc -l`
 TOTALEN=`grep -v ">" $ASSEMBLY | awk '{c+=length($1)}END{print c}'`
-sed "s/ecol/$PREFIX/g;s/>1</>$SEQCNT</1;s/>4670116</>$TOTALEN</1" /root/Assemblosis/referenceset.xml > /root/Assemblosis/referenceset.xml > /root/smrtlink/install/smrtlink-release_5.0.1.9585/bundles/smrttools/install/smrttools-release_5.0.1.9578/private/pacbio/pythonpkgs/pbcore/lib/python2.7/site-packages/pbcore/data/datasets/$PREFIX/referenceset.xml
+sed "s/ecol/$PREFIX/g;s/>1</>$SEQCNT</1;s/>4670116</>$TOTALEN</1" /root/Assemblosis/referenceset.xml > /root/smrtlink/install/smrtlink-release_5.0.1.9585/bundles/smrttools/install/smrttools-release_5.0.1.9578/private/pacbio/pythonpkgs/pbcore/lib/python2.7/site-packages/pbcore/data/datasets/$PREFIX/referenceset.xml
 
 # Convert hdf5 files to subread format understood by pbsmrtpipe
 echo "python /root/Assemblosis/createFofn.py -d $DATADIR -f baxFiles.fofn"
